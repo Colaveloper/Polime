b206c288e044d5abf074b65deb879ed3021be386
Colaveloper <63930332+Colaveloper@users.noreply.github.com>
2020-10-04 15:09:07 +0200

    Made AppNavigato-Header and FabButton, plus a little lovely counter
    The Fab Button is meant to create the daily js dataset with all the scores. In the main page there will be previous days scores and data-analysis

--- POLIME/App.js
+++ POLIME/App.js
@@ -1,29 +1,72 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class HomeScreen extends React.Component {
+  state = {
+    counter: 0,
+  };
 
-const App = () => {
-  return (
-    <>
-      <Text>Let's dev some shit</Text>
-    </>
-  );
-};
+  render() {
+    const counter = this.state.counter;
 
-const styles = StyleSheet.create({});
+    return (
+      <View style={styles.mainContainer}>
+        <Text style={styles.counter}>Counter: {counter}</Text>
+        <TouchableOpacity style={styles.fab} onPress={this.onIncrement}>
+          <Icon name="plus" size={30} color="#150" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
-export default App;
+  onIncrement = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+}
+
+//this is the HEADER
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: () => ({
+      title: 'Polime',
+      headerTitleStyle: {
+        textAlign: 'center',
+      },
+    }),
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default function App() {
+  return <AppContainer />;
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  counter: {
+    fontSize: 25,
+  },
+  fab: {
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.3)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 15,
+    right: 15,
+    height: 60,
+    width: 60,
+    borderRadius: 100,
+  },
+});
