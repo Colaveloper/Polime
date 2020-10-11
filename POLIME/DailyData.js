f9395669732774e064fbe3ba5d448b187da639ea
Colaveloper <63930332+Colaveloper@users.noreply.github.com>
2020-10-11 18:59:27 +0200

    Now sliders show their value, added save button
    no data is currently passed over screens or actually saved

--- POLIME/DailyData.js
+++ POLIME/DailyData.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, Button} from 'react-native';
 import Slider from '@react-native-community/slider';
 
 export default class DailyData extends Component {
@@ -9,31 +9,64 @@ export default class DailyData extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Creativity</Text>
-        <GeneralSlider />
-        <GeneralSlider />
-        <GeneralSlider />
-        <GeneralSlider />
-        <GeneralSlider />
+      <View style={{flex: 1}}>
+        <GeneralSlider thumbColor="#02b881" type="physical" />
+        <GeneralSlider thumbColor="#1a09d6" type="creative" />
+        <GeneralSlider thumbColor="#d60954" type="learning" />
+        <GeneralSlider thumbColor="#d68409" type="social" />
+        <GeneralSlider thumbColor="#d6f000" type="self-care" />
+        <View style={styles.button}>
+          <Button title="SAVE" onPress={this.goToHome}></Button>
+        </View>
       </View>
+    );
+  }
+
+  goToHome = () => {
+    this.props.navigation.navigate('Home');
+  };
+}
+
+class GeneralSlider extends Component {
+  static defaultProps = {
+    value: 0,
+  };
+
+  state = {
+    value: this.props.value,
+  };
+
+  render() {
+    return (
+      <>
+        <Slider
+          style={styles.slider}
+          minimumValue={0}
+          maximumValue={20}
+          step={1}
+          thumbTintColor={this.props.thumbColor}
+          minimumTrackTintColor="#000000"
+          maximumTrackTintColor="#b8b8b8"
+          onValueChange={(value) => this.setState({value: value})}
+        />
+        <Text style={styles.text}>{this.props.type}</Text>
+        <Text style={styles.text}>
+          {this.state.value && +this.state.value.toFixed(3)}
+        </Text>
+      </>
     );
   }
 }
 
-class GeneralSlider extends Component {
-  render() {
-    return (
-      <Slider
-        style={{width: 400, height: 40}}
-        minimumValue={0}
-        maximumValue={20}
-        step={1}
-        thumbTintColor="#5eff6c"
-        minimumTrackTintColor="#218229"
-        maximumTrackTintColor="#b8b8b8"
-        maximumTrackImage
-      />
-    );
-  }
-}
+const styles = StyleSheet.create({
+  slider: {width: 360, height: 40, marginLeft: 20},
+  text: {
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '500',
+    margin: 10,
+  },
+  button: {
+    marginHorizontal: 14,
+  },
+});
