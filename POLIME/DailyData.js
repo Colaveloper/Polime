be0341a753dca393647a762de911446b0ca6f407
Colaveloper <63930332+Colaveloper@users.noreply.github.com>
2021-02-15 01:01:24 +0100

    Created an object for an aspect scores, max scores, and goals
    This is a necessary step to let the sliders know their props, and changing them in-app rather than in vs-code

--- POLIME/DailyData.js
+++ POLIME/DailyData.js
@@ -3,6 +3,45 @@ import {StyleSheet, Text, Button, Pressable, View} from 'react-native';
 import {color} from 'react-native-reanimated';
 import GeneralSlider from './GeneralSlider';
 
+// In the arrays that follow, the first number tells the score,
+// while the second tells its max value.
+
+// Default is the sub-slider that represent
+// a general aspect (e.g. active) and not a
+// goal, and therefore has its max value calculated as
+// 20 - all the goals max weight
+
+// Score is calculated as the sum of default and goals scores
+
+var active = {
+  goals: {
+    goalOne: [2, 8],
+    goalTwo: [2, 2],
+  },
+
+  defaultScore: 1,
+
+  default: function () {
+    var sum = 0;
+    for (var goal in this.goals) {
+      if (this.goals.hasOwnProperty(goal)) {
+        sum += parseFloat(this.goals[goal][1]);
+      }
+    }
+    return [this.defaultScore, 20 - sum];
+  },
+
+  score: function () {
+    var sum = 0;
+    for (var goal in this.goals) {
+      if (this.goals.hasOwnProperty(goal)) {
+        sum += parseFloat(this.goals[goal][0]);
+      }
+    }
+    return sum;
+  },
+};
+
 export default class DailyData extends Component {
   state = {
     showOnlySlider: 'all',
